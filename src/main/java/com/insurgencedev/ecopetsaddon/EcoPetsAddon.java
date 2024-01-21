5ef61c206606111a89a85897e2b1ce6e272c2d68
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-21 01:48:01 -0500

    updated multi handling

--- src/main/java/com/insurgencedev/ecopetsaddon/EcoPetsAddon.java
+++ src/main/java/com/insurgencedev/ecopetsaddon/EcoPetsAddon.java
@@ -1,14 +1,17 @@
 package com.insurgencedev.ecopetsaddon;
 
 import com.insurgencedev.ecopetsaddon.listeners.EcoPetsEventListener;
+import org.bukkit.Bukkit;
 import org.insurgencedev.insurgenceboosters.api.addon.IBoostersAddon;
 import org.insurgencedev.insurgenceboosters.api.addon.InsurgenceBoostersAddon;
 
-@IBoostersAddon(name = "EcoPetsAddon", version = "1.0.0", author = "InsurgenceDev", description = "EcoPets Support")
+@IBoostersAddon(name = "EcoPetsAddon", version = "1.0.1", author = "InsurgenceDev", description = "EcoPets Support")
 public class EcoPetsAddon extends InsurgenceBoostersAddon {
 
     @Override
     public void onAddonReloadablesStart() {
-        registerEvent(new EcoPetsEventListener());
+        if (Bukkit.getPluginManager().isPluginEnabled("EcoPets")) {
+            registerEvent(new EcoPetsEventListener());
+        }
     }
 }
