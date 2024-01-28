cd6c2b779eebc583a365eb3a8777367e3de861a6
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-28 00:03:55 -0500

    api jar update

--- src/main/java/com/insurgencedev/ecopetsaddon/EcoPetsAddon.java
+++ src/main/java/com/insurgencedev/ecopetsaddon/EcoPetsAddon.java
@@ -1,16 +1,16 @@
 package com.insurgencedev.ecopetsaddon;
 
 import com.insurgencedev.ecopetsaddon.listeners.EcoPetsEventListener;
-import org.bukkit.Bukkit;
 import org.insurgencedev.insurgenceboosters.api.addon.IBoostersAddon;
 import org.insurgencedev.insurgenceboosters.api.addon.InsurgenceBoostersAddon;
+import org.insurgencedev.insurgenceboosters.libs.fo.Common;
 
 @IBoostersAddon(name = "EcoPetsAddon", version = "1.0.1", author = "InsurgenceDev", description = "EcoPets Support")
 public class EcoPetsAddon extends InsurgenceBoostersAddon {
 
     @Override
-    public void onAddonReloadablesStart() {
-        if (Bukkit.getPluginManager().isPluginEnabled("EcoPets")) {
+    public void onAddonReloadAblesStart() {
+        if (Common.doesPluginExist("EcoPets")) {
             registerEvent(new EcoPetsEventListener());
         }
     }
