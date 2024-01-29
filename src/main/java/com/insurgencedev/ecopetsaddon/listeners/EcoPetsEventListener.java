6a3309c4f81c2f1de142449ae32dd8a608279d67
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-28 21:47:46 -0500

    fixed multi

--- src/main/java/com/insurgencedev/ecopetsaddon/listeners/EcoPetsEventListener.java
+++ src/main/java/com/insurgencedev/ecopetsaddon/listeners/EcoPetsEventListener.java
@@ -12,7 +12,7 @@ public final class EcoPetsEventListener implements Listener {
     private void onGain(PlayerPetExpGainEvent event) {
         final String TYPE = "Pets";
         final String NAMESPACE = "ECO_PETS";
-        final double[] totalMulti = {1};
+        final double[] totalMulti = {0};
 
         BoosterFindResult pResult = IBoosterAPI.INSTANCE.getCache(event.getPlayer()).getBoosterDataManager().findActiveBooster(TYPE, NAMESPACE);
         if (pResult instanceof BoosterFindResult.Success boosterResult) {
@@ -24,7 +24,9 @@ public final class EcoPetsEventListener implements Listener {
             return null;
         }, () -> null);
 
-        event.setAmount(calculateAmount(event.getAmount(),totalMulti[0]));
+        if (totalMulti[0] > 0) {
+            event.setAmount(calculateAmount(event.getAmount(), totalMulti[0]));
+        }
     }
 
     private long calculateAmount(double amount, double multi) {
